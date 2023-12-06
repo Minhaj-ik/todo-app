e3b66cbab404e7916384bb2bbd502a3927038607
Minhaj I K <minhajismailkhan@gmail.com>
2023-12-06 16:57:43 +0530

    creating a todo app with react

---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <>
+        <div className='container'>
+            <div className='Header'>Todo App</div>
+            <div className='add-task'>Add task</div>
+            <div className='tasks'>List task</div>
+        </div>
+    </>
+  )
+}
+
+export default Todo
